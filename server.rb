16f2f463695d83018bcaa47a5bc7a698b329eecf
Brandon Vergara <brandon@makingdevs.com>
2020-04-02 01:26:34 -0600

    coding master  adding server0

--- server.rb
+++ server.rb
@@ -6,16 +6,28 @@ require 'active_record'
 ActiveRecord::Base.establish_connection(
   :adapter  => "mysql2",
   :host     => "localhost",
-  :username => "rot",
+  :username => "root",
   :password => "makingdevs",
   :database => "sinatraApi"
 )
 
+class User < ActiveRecord::Base
+end
+
+ActiveRecord::Migration.create_table :users, if_not_exists: true do |t|
+  t.string :name
+  t.string :username
+end
+
+User.create(name: 'Brandon', username: "brandonVergara")
+
+class App < Sinatra::Application
+end
 # Endpoints
 get '/ ' do
   'Welcome to BookList!'
 end
 
-get '/hola' do
-  "Hello World"
+get '/users' do
+  User.all.to_json
 end
